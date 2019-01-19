92452c27f6ec45c18bcd01dbe26bb2169ef35e41
Nicky Moelholm <moelholm@gmail.com>
2019-01-19 16:14:05 +0100

    added eslint with airbnb rules

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
-'use strict';
+const request = require('request');
+const tableify = require('tableify');
 
-var request = require('request');
-var tableify = require('tableify');
-
-const parseSnsRecord = (snsRecord) => ({
-    "MessageId": snsRecord.MessageId,
-    "TopicArn": snsRecord.TopicArn,
-    "Subject": snsRecord.Subject,
-    "Message": snsRecord.Message ? JSON.parse(snsRecord.Message) : null
+const parseSnsRecord = snsRecord => ({
+  MessageId: snsRecord.MessageId,
+  TopicArn: snsRecord.TopicArn,
+  Subject: snsRecord.Subject,
+  Message: snsRecord.Message ? JSON.parse(snsRecord.Message) : null,
 });
 
 exports.handler = (event, context, callback) => {
-    console.log('[INFO] Handler invoked. Event details:', event);
+  console.log('[INFO] Handler invoked. Event details:', event);
 
-    // Extract event payload (records)
-    const eventRecords = (event.Records ? event.Records : []);
-    console.log('[INFO] Event records: ', eventRecords);
+  // Extract event payload (records)
+  const eventRecords = (event.Records ? event.Records : []);
+  console.log('[INFO] Event records: ', eventRecords);
 
-    // Format SNS records as HTML tables
-    const snsRecordStrings = eventRecords
-        .filter(record => record.Sns)
-        .map(record => parseSnsRecord(record.Sns))
-        .map(record => tableify(record));
+  // Format SNS records as HTML tables
+  const snsRecordStrings = eventRecords
+    .filter(record => record.Sns)
+    .map(record => parseSnsRecord(record.Sns))
+    .map(record => tableify(record));
 
-    // Abort if we didn't receive proper SNS records
-    if (snsRecordStrings.length === 0) {
-        console.log('[ERROR] Skipping event because it does not contain any SNS records');
-        callback("error");
-    }
+  // Abort if we didn't receive proper SNS records
+  if (snsRecordStrings.length === 0) {
+    console.log('[ERROR] Skipping event because it does not contain any SNS records');
+    callback('error');
+  }
 
-    // Prepare HTTP request
-    const httpRequest = {
-        uri: process.env.TEAMS_WEB_HOOK_URL,
-        method: 'POST',
-        json: {
-            "text": snsRecordStrings.toString()
-        }
-    };
+  // Prepare HTTP request
+  const httpRequest = {
+    uri: process.env.TEAMS_WEB_HOOK_URL,
+    method: 'POST',
+    json: {
+      text: snsRecordStrings.toString(),
+    },
+  };
 
-    // Send HTTP request
-    console.log('[INFO] Posting to Webhook...');
-    request(httpRequest, (error, response) => {
-        const statusCode = (response && response.statusCode) ? response.statusCode : null;
-        const logLevel = (statusCode === 200) ? 'INFO' : 'ERROR';
-        const logMessage = `[${logLevel}] Posting to Webhook...done. HTTP status code: [${statusCode}]`;
-        console.log(logMessage, error ? error : '');
-        callback(error, "done");
-    });
+  // Send HTTP request
+  console.log('[INFO] Posting to Webhook...');
+  request(httpRequest, (error, response) => {
+    const statusCode = (response && response.statusCode) ? response.statusCode : null;
+    const logLevel = (statusCode === 200) ? 'INFO' : 'ERROR';
+    const logMessage = `[${logLevel}] Posting to Webhook...done. HTTP status code: [${statusCode}]`;
+    console.log(logMessage, error || '');
+    callback(error, 'done');
+  });
 };
