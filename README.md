a0b88da297db6d975ce424ab8e991ebb7844688b
Nicky Moelholm <moelholm@gmail.com>
2019-01-19 15:37:57 +0100

    initial import

--- README.md
+++
@@ -1,2 +0,0 @@
-# aws-cloudwatch-alarms-to-microsoft-teams-alerter
-An AWS Lambda function that can be used to push CloudWatch Alarms to a Microsoft Teams channel
---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var request = require('request');
+var tableify = require('tableify');
+
+const parseSnsRecord = (snsRecord) => ({
+    "MessageId": snsRecord.MessageId,
+    "TopicArn": snsRecord.TopicArn,
+    "Subject": snsRecord.Subject,
+    "Message": snsRecord.Message ? JSON.parse(snsRecord.Message) : null
+});
+
+exports.handler = (event, context, callback) => {
+    console.log('[INFO] Handler invoked. Event details:', event);
+
+    // Extract event payload (records)
+    const eventRecords = (event.Records ? event.Records : []);
+    console.log('[INFO] Event records: ', eventRecords);
+
+    // Format SNS records as HTML tables
+    const snsRecordStrings = eventRecords
+        .filter(record => record.Sns)
+        .map(record => parseSnsRecord(record.Sns))
+        .map(record => tableify(record));
+
+    // Abort if we didn't receive proper SNS records
+    if (snsRecordStrings.length === 0) {
+        console.log('[ERROR] Skipping event because it does not contain any SNS records');
+        callback("error");
+    }
+
+    // Prepare HTTP request
+    const httpRequest = {
+        uri: process.env.TEAMS_WEB_HOOK_URL,
+        method: 'POST',
+        json: {
+            "text": snsRecordStrings.toString()
+        }
+    };
+
+    // Send HTTP request
+    console.log('[INFO] Posting to Webhook...');
+    request(httpRequest, (error, response) => {
+        const statusCode = (response && response.statusCode) ? response.statusCode : null;
+        const logLevel = (statusCode === 200) ? 'INFO' : 'ERROR';
+        const logMessage = `[${logLevel}] Posting to Webhook...done. HTTP status code: [${statusCode}]`;
+        console.log(logMessage, error ? error : '');
+        callback(error, "done");
+    });
+};
