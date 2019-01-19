60016a05c777d54d512ed4f7ed830dce582e0826
Nicky Moelholm <moelholm@gmail.com>
2019-01-19 17:06:12 +0100

    refactored handler

--- index.js
+++ index.js
@@ -8,6 +8,38 @@ const parseSnsRecord = snsRecord => ({
   Message: snsRecord.Message ? JSON.parse(snsRecord.Message) : null,
 });
 
+const formatAsHtmlTables = eventRecords => eventRecords
+  .filter(record => record.Sns)
+  .map(record => parseSnsRecord(record.Sns))
+  .map(record => tableify(record));
+
+const sendToMicrosoftTeams = (snsRecordStrings, callback) => {
+  // Prepare HTTP request
+  const httpRequestConfig = {
+    uri: process.env.TEAMS_WEB_HOOK_URL,
+    method: 'POST',
+    json: {
+      text: snsRecordStrings.toString(),
+    },
+  };
+  // Send HTTP request
+  console.log('[INFO] Posting to Webhook...');
+  request(httpRequestConfig, (error, response) => {
+    const statusCode = (response && response.statusCode) ? response.statusCode : null;
+    const logLevel = (statusCode === 200) ? 'INFO' : 'ERROR';
+    const logMessage = `[${logLevel}] Posting to Webhook...done. HTTP status code: [${statusCode}]`;
+    console.log(logMessage, error || '');
+    callback(error, 'done');
+  });
+};
+
+//
+// AWS Lambda handler (main function).
+//
+// Receives messages from an SNS topic containing SNS records.
+// Formats them as HTML tables.
+// Forwards them to Microsoft Teams via a Webhook.
+//
 exports.handler = (event, context, callback) => {
   console.log('[INFO] Handler invoked. Event details:', event);
 
@@ -16,10 +48,7 @@ exports.handler = (event, context, callback) => {
   console.log('[INFO] Event records: ', eventRecords);
 
   // Format SNS records as HTML tables
-  const snsRecordStrings = eventRecords
-    .filter(record => record.Sns)
-    .map(record => parseSnsRecord(record.Sns))
-    .map(record => tableify(record));
+  const snsRecordStrings = formatAsHtmlTables(eventRecords);
 
   // Abort if we didn't receive proper SNS records
   if (snsRecordStrings.length === 0) {
@@ -29,22 +58,6 @@ exports.handler = (event, context, callback) => {
     return;
   }
 
-  // Prepare HTTP request
-  const httpRequest = {
-    uri: process.env.TEAMS_WEB_HOOK_URL,
-    method: 'POST',
-    json: {
-      text: snsRecordStrings.toString(),
-    },
-  };
-
   // Send HTTP request
-  console.log('[INFO] Posting to Webhook...');
-  request(httpRequest, (error, response) => {
-    const statusCode = (response && response.statusCode) ? response.statusCode : null;
-    const logLevel = (statusCode === 200) ? 'INFO' : 'ERROR';
-    const logMessage = `[${logLevel}] Posting to Webhook...done. HTTP status code: [${statusCode}]`;
-    console.log(logMessage, error || '');
-    callback(error, 'done');
-  });
+  sendToMicrosoftTeams(snsRecordStrings, callback);
 };
